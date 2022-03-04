da42e1d134e38dc5b96c9caf94edb80ad0165b53
cklewin <cklewin@users.noreply.github.com>
2022-03-04 19:58:36 +0000

    Release 3.1.7

--- src/testdir/__about__.py
+++ src/testdir/__about__.py
@@ -10,6 +10,6 @@ __all__ = [
 ]
 
 __title__ = 'test'
-__version__ = '3.2.0-alpha'
+__version__ = '3.1.7'
 __license__ = 'Copyright (c) 2022 Jevaka.org'
 __copyright__ = 'Copyright (c) 2022 Jevaka.org'
